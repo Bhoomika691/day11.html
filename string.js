fd04e5dafceeb7a071977905e4ef20a2577a6d00
Bhoomika691 <kbhoomika691@gmail.com>
2025-07-16 10:26:29 +0530

    created string.js
    string operations like trim,uppercase,lowercase,slice,replace and repeat

---
+++ string.js
@@ -0,0 +1,11 @@
+a="   hello   "
+a.trim();
+b=a.trim();
+console.log(b);
+console.log(b.toUpperCase());
+d=b.slice(2,4);
+console.log(d);
+e=b.replace("hello","welcome");
+console.log(e);
+f=e.repeat(3);
+console.log(f);
