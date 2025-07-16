7585c8041f47d1790350aebfddf0de742becd933
Bhoomika691 <kbhoomika691@gmail.com>
2025-07-16 10:29:32 +0530

    created conditional.js
    added prompts and if,else conditions

---
+++ conditional.js
@@ -0,0 +1,27 @@
+/*a=5
+b=6
+console.log(a>b);
+console.log(a<b);
+console.log(a==b);
+console.log(a!=b);
+console.log(a>=b);
+console.log(a<=b);*/
+/*NAN,empty,null,undefined,0 for these values false*/
+/*if(50){
+    console.log("true");
+}
+else{
+    console.log("false");
+}*/
+/*console.error("this is my error");
+console.warn("enter your name");*/
+let f=prompt("enter your name");
+let l=prompt("enter your branch");
+/*console.log(f);
+console.log(l);*/
+console.log("welcome",f,l,"!");
+var flag=confirm("are you sure");
+if(flag==true)
+    console.log("clicked on ok")
+else
+    console.log("clicked on cancel")
