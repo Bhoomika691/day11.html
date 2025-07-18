4f43df175571dc5cc3caa10377d74988be7d2c86
Bhoomika691 <kbhoomika691@gmail.com>
2025-07-18 10:13:15 +0530

    created randomgame.js
    added some if and else conditions

---
+++ randomgame.js
@@ -0,0 +1,36 @@
+/*let n=prompt("enter a max value");
+let random=Math.ceil(Math.random()*n);
+let guess=prompt("guess a number");
+while(true){
+    if(guess!=random){
+        guess=prompt("wrong guess");
+    }
+    if(guess==random){
+        console.log("congrats");
+        break;
+    }
+    if(guess=="quit"){
+        console.log("your quit");
+        break;
+    }
+}*/
+/*let n=prompt("enter a max value");
+let random=Math.ceil(Math.random()*n);
+let guess=prompt("guess a number");
+while(true){
+    if(guess>random){
+        guess=prompt("guess is big guess again");
+    }
+    else if(guess==random){
+        console.log("congrats");
+        break;
+    }
+    else if(guess<random)
+    {
+        guess=prompt("guess is small guess again");
+    }
+    if(guess=="quit"){
+        console.log("your quit");
+        break;
+    }
+}*/
