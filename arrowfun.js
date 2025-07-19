0086d98c1b396a94109c1da396104467c878b594
Bhoomika691 <kbhoomika691@gmail.com>
2025-07-19 09:40:36 +0530

    created arrowfun.js
    added let conditions

---
+++ arrowfun.js
@@ -0,0 +1,20 @@
+/*const student={
+    eng:95,bio:85,
+
+}
+let getavg=()=>
+{
+    console.log("avg");
+}*/
+console.log("hi there")
+let id1=setInterval(()=>{
+    console.log("hello");
+},2000);
+let id2=setInterval(()=>{
+    console.log("hii");
+},2000);
+clearInterval(id1);
+console.log(id1);
+console.log(id2);
+
+
