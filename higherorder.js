7c5b7a8d3309c565115057cafae1ba267c02e12a
Bhoomika691 <kbhoomika691@gmail.com>
2025-07-19 09:39:16 +0530

    created higherorder.js
    added if and else

---
+++ higherorder.js
@@ -0,0 +1,14 @@
+function oddeven(req)
+{
+    if(req=="odd")
+    {
+        let odd=function(n){
+            console.log(n%2==0);
+        }
+        return even;
+    }
+    else{
+        console.log("wrong req");
+    }
+}
+let n=100;
