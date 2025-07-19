78dc46cd246aef817a14f762d471facc92441d47
Bhoomika691 <kbhoomika691@gmail.com>
2025-07-19 09:38:02 +0530

    created methodsin fun.js
    added add,sub,mul operations

---
+++ methodsin fun.js
@@ -0,0 +1,17 @@
+const cal={
+    add(a,b)
+    {
+        return a+b;
+    },
+    sub:function(a,b)
+    {
+        return a-b;
+    },
+    mul:function(a,b)
+    {
+        return a*b;
+    }
+}
+console.log(cal.add(3,4));
+console.log(cal.sub(3,4));
+console.log(cal.mul(3,4));
