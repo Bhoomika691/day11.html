212156a61e6a44cb8483c9381e6ec41d60248647
Bhoomika691 <kbhoomika691@gmail.com>
2025-07-26 15:50:12 +0530

    updated this.js
    added some more features

--- this.js
+++ this.js
@@ -5,7 +5,7 @@
         console.log(student.getavg());
     }
 }*/
-const student={
+/*const student={
     eng:95,
     bio:85,
     
@@ -16,4 +16,25 @@ const student={
     {
         console.log(this);
     }
+}*/
+/*function sum(a=1,b)
+{
+    return a+b;
 }
+console.log(sum(2));*/
+/*console.log(..."bhoomi")
+arr1=[1,2,5,8,9,-10,-20,5,-50]
+arr1
+/*(10) [1, 2, 5, 8, 9, -10, -20, 5, -50, 45]*/
+arr1.push(45);/*11*/
+let char=[..."alice"]
+char/*(5) ['a', 'l', 'i', 'c', 'e']*/
+const obj1={
+name:"bhoomi",age:19}
+console.log(obj1)/*{name: 'bhoomi', age: 19}*/
+const copy={...obj1}
+console.log(copy)/*{name: 'bhoomi', age: 19}*/
+const obj2={...arr}
+obj2
+const obj3={..."bhoomi"}
+console.log(obj3);
